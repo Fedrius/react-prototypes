0b49783abd491c49bb7e23e9cc0d69626c008712
Paul <paulmlee7@gmail.com>
2018-01-29 14:32:02 -0800

    completed feature set 1

---
+++ src/components/scrap.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './scrapbook.css'
+
+const Scrapbook = ()=>{
+    return (
+        <div className='scrapbook-container'>scrappycoco</div>
+    )
+}
+
+export default Scrapbook
