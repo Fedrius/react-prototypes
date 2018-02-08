8d5efefa987b38347de63e90a8e8186981fc268a
Paul <paulmlee7@gmail.com>
2018-02-08 15:47:13 -0800

    started feature set 2

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,12 @@
+import React, {Component} from 'react';
+import Clock from './clock';
 
+export default ()=> {
+    return (
+        <div className='container'>
+            <h1 className='text-center'>REDUX CLOX</h1>
+            <Clock/>
+        </div>
+    )
+}
 
---
+++ src/components/clock.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Clock extends Component {
+
+    render(){
+        return (
+            <div className="text-center mt-5">
+                <h1>some text</h1>
+            </div>
+        )
+    }
+}
+
+export default Clock;
